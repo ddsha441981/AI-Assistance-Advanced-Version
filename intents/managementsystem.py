5288c1d5e41de752ebb713d3439951c452eeb305
ddsha441981 <ddsa44191@gmail.com>
2021-04-08 22:23:18 +0530

    we can manage files of any directory by using my assistant with help of my voice

--- intents/managementsystem.py
+++ intents/managementsystem.py
@@ -8,9 +8,9 @@ __license__ = ""
 __status__ = ""
 
 import os
-
 import psutil
-
+import sys
+import shutil
 from utils.utils import Utils
 
 
@@ -83,7 +83,8 @@ class ManagementSystem:
             elif percentage <= 15 or percentage <= 30:
                 self.response_speak("we don't have enough power to contiune work, please connect charging")
             elif percentage <= 15:
-                self.response_speak("we have very low power, Please connect to charging, the system will be shutdown very soon")
+                self.response_speak(
+                    "we have very low power, Please connect to charging, the system will be shutdown very soon")
         except Exception as e:
             print(e)
 
@@ -167,14 +168,78 @@ class ManagementSystem:
         else:
             if path in 'shutdown':
                 self.response_speak(self.response)
-                print("inside shutdown method")
                 self.shutdown(self.os_name)
+
             elif path in 'restart':
                 self.restart_pc()
+
             elif path in 'sleep':
                 self.sleep_pc()
+
             elif path in 'battery':
                 self.battery_power()
+
             elif path in 'cpu':
                 self.cpu_usage()
+            elif path in 'manage files' or path in 'manage data':
+                EXT_VIDEO_LIST = ['FLV', 'WMV', 'MOV', 'MP4', 'MPEG', '3GP', 'MKV', 'AVI']
+                EXT_IMAGE_LIST = ['JPG', 'JPEG', 'GIF', 'PNG', 'SVG']
+                EXT_DOCUMENT_LIST = ['DOC', 'DOCX', 'PPT', 'PPTX', 'PAGES', 'PDF', 'ODT', 'ODP', 'XLSX', 'XLS', 'ODS',
+                                     'TXT', 'IN',
+                                     'OUT', 'MD']
+                EXT_MUSIC_LIST = ['MP3', 'WAV', 'WMA', 'MKA', 'AAC', 'MID', 'RA', 'RAM', 'RM', 'OGG']
+                EXT_CODE_LIST = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS']
+                EXT_EXECUTABLE_LIST = ['LNK', 'DEB', 'EXE', 'SH', 'BUNDLE']
+                EXT_COMPRESSED_LIST = ['RAR', 'JAR', 'ZIP', 'TAR', 'MAR', 'ISO', 'LZ', '7ZIP', 'TGZ', 'GZ', 'BZ2']
+
+                # Taking the location of the Folder to Arrange
+                try:
+                    destLocation = str(sys.argv[1])
+                except IndexError:
+                    self.response_speak("Enter the Path of directory")
+                    destLocation = str(input('Enter here...... : '))
+
+                # When we make a folder that already exist then WindowsError happen
+                # Changing directory may give WindowsError
+                def ChangeDirectory(dir):
+                    try:
+                        os.chdir(dir)
+                    except WindowsError:
+                        print('Error! Cannot change the Directory')
+                        print('Enter a valid directory!')
+                        ChangeDirectory(str(input('Enter the Path of directory: ')))
+
+                ChangeDirectory(destLocation)
+
+                def Organize(dirs, name):
+                    try:
+                        os.mkdir(name)
+                        print('{} Folder Created'.format(name))
+                    except WindowsError:
+                        print('{} Folder Exist'.format(name))
+
+                    src = '{}\\{}'.format(destLocation, dirs)
+                    dest = '{}\{}'.format(destLocation, name)
+
+                    os.chdir(dest)
+                    shutil.move(src, '{}\\{}'.format(dest, dirs))
+
+                    print(os.getcwd())
+                    os.chdir(destLocation)
+
+                TYPES_LIST = ['Video', 'Images', 'Documents', 'Music', 'Codes', 'Executables', 'Compressed']
+                for dirs in os.listdir(os.getcwd()):
+                    if 1:
+                        for name, extensions_list in zip(TYPES_LIST,
+                                                         [EXT_VIDEO_LIST, EXT_IMAGE_LIST, EXT_DOCUMENT_LIST,
+                                                          EXT_MUSIC_LIST,
+                                                          EXT_CODE_LIST, EXT_EXECUTABLE_LIST, EXT_COMPRESSED_LIST]):
+                            if dirs.split('.')[-1].upper() in extensions_list:
+                                Organize(dirs, name)
+                    else:
+                        if dirs not in TYPES_LIST:
+                            Organize(dirs, 'Folders')
+                self.response_speak("task completed..")
+                self.response_speak('Done Arranging Files and Folder in your specified directory')
+
         print(str(path))
